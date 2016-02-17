995b76e68eab80128cdf3eae9285dc8365c4c7e8
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2016-02-16 22:52:50 -0300

    Melhorias no template
    Template

--- apreciacoes/settings.py
+++ apreciacoes/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'reconhecimentos'
 ]
 
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'UTC'
 
